3714ed04c0b75871903492455d4e43f01626b344
Sathvik Katam <katam.sathvik@gmail.com>
2018-05-28 14:59:06 +0200

    intendation fix

--- python/app.py
+++ python/app.py
@@ -14,7 +14,9 @@ def hello():
     visits = redis.incr("counter")
   except RedisError:
     visits = "<i>cannot connect to Redis, counter disabled</i>"
+
   html = "<h3>Hello {name}!</h3> <b>Hostname:</b> {hostname}<br/> <b>Visits:</b> {visits}"
+
   return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main__":
